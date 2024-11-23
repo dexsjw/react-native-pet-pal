d2471629e853d19d62b4f375b96f1a01b699925b
dextersjw <dextersjw@gmail.com>
2024-11-23 12:37:54 +0800

    added comments for mock api

--- service/api/PetPalMockApi.js
+++ service/api/PetPalMockApi.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// Do not use localhost, use your machine's IPv4 address instead
+// Reason: Our phone / emulator is not on the same "localhost" as our machine
+
+// const BASE_URL = "http://localhost";
 const BASE_URL = "http://10.91.208.37";
 const PORT = ":3001";
 
