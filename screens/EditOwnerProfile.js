e8c79733c119c13c5d77d6cd071fd434722bc4f8
dextersjw <dextersjw@gmail.com>
2024-11-23 08:12:20 +0800

    added dummy edit owner profile page

---
+++ screens/EditOwnerProfile.js
@@ -0,0 +1,21 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+function EditOwnerProfile() {
+  
+  return (
+    <View style={{
+      flex: 1,
+      padding: 10,
+      justifyContent: "center",
+      alignContent: "center"
+    }}>
+      <Text 
+        variant="displayMedium"
+      >
+        Edit owner profile feature to be implemented...
+      </Text>
+    </View>  )
+}
+
+export default EditOwnerProfile;
