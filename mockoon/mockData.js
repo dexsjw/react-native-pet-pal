6cc732b83ca8f257fc1e13075f87c0d234bfca07
dextersjw <dextersjw@gmail.com>
2024-11-23 08:11:40 +0800

    added mock data due to issue with axios

---
+++ mockoon/mockData.js
@@ -0,0 +1,200 @@
+export const mockOwnerAuths = [
+  {
+    "id": 1,
+    "email": "admin@petpal.com",
+    "password": "ForAdmin0nly!",
+    "ownerId": 1
+  },
+  {
+    "id": 6501169140888877,
+    "email": "Maximilian13@yahoo.com",
+    "password": "Ante.",
+    "ownerId": 8221104367580667
+  },
+  {
+    "id": 1876777330252800,
+    "email": "Roxanne_Lueilwitz@yahoo.com",
+    "password": "Degero.",
+    "ownerId": 652266505789337
+  },
+  {
+    "id": 7674735878883155,
+    "email": "Winifred_Bradtke76@hotmail.com",
+    "password": "Articulus.",
+    "ownerId": 6342473330020863
+  }
+]
+
+export const mockOwners = [
+  {
+    "id": 1,
+    "name": "Laury Zemlak",
+    "ownerMatches": [],
+    "areaLocation": "870 Block Hollow",
+    "ownerAuth": 1,
+    "pets": [
+      {
+        "id": 4633830215693110,
+        "name": "Valentine",
+        "gender": "female",
+        "age": 10,
+        "isNeutered": false,
+        "pictures": [
+          "",
+          "",
+          ""
+        ],
+        "description": "Eaque arguo quae debilito pecco umerus cibus titulus capillus nobis caute abeo tam ambitus vitium aegre tremo nesciunt solio textilis.",
+        "petData": 1
+      }
+    ]
+  },
+  {
+    "id": 8221104367580667,
+    "name": "Daren Bruen",
+    "ownerMatches": [],
+    "areaLocation": "642 N Oak Street",
+    "ownerAuth": 6501169140888877,
+    "pets": [
+      {
+        "id": 7573242285295693,
+        "name": "Nelson",
+        "gender": "male",
+        "age": 1,
+        "isNeutered": false,
+        "pictures": [
+          "",
+          ""
+        ],
+        "description": "Pecus deleo aetas corrumpo voluptates voluptas caput contego admitto arca arca cruciamentum bellicus assumenda certe censura officiis magni damnatio centum.",
+        "petData": 1
+      }
+    ]
+  },
+  {
+    "id": 652266505789337,
+    "name": "Royal Wolf",
+    "ownerMatches": [],
+    "areaLocation": "724 Vandervort Villages",
+    "ownerAuth": 1876777330252800,
+    "pets": [
+      {
+        "id": 2571283212380844,
+        "name": "Sebastian",
+        "gender": "female",
+        "age": 6,
+        "isNeutered": false,
+        "pictures": [
+          ""
+        ],
+        "description": "Vulpes capio appono conturbo demonstro arbor itaque comprehendo velociter beneficium cerno exercitationem solitudo damnatio voluptas pauci maiores adopto barba vigilo.",
+        "petData": 1
+      }
+    ]
+  },
+  {
+    "id": 6342473330020863,
+    "name": "Alicia Herzog",
+    "ownerMatches": [],
+    "areaLocation": "254 Lehner Route",
+    "ownerAuth": 7674735878883155,
+    "pets": [
+      {
+        "id": 4633830215693110,
+        "name": "Valentine",
+        "gender": "female",
+        "age": 10,
+        "isNeutered": false,
+        "pictures": [
+          "",
+          "",
+          ""
+        ],
+        "description": "Eaque arguo quae debilito pecco umerus cibus titulus capillus nobis caute abeo tam ambitus vitium aegre tremo nesciunt solio textilis.",
+        "petData": 1
+      }
+    ]
+  }
+]
+
+export const mockPets = [
+  {
+    "id": 2571283212380844,
+    "name": "Sebastian",
+    "gender": "female",
+    "age": 6,
+    "isNeutered": false,
+    "pictures": [
+      ""
+    ],
+    "description": "Vulpes capio appono conturbo demonstro arbor itaque comprehendo velociter beneficium cerno exercitationem solitudo damnatio voluptas pauci maiores adopto barba vigilo.",
+    "petData": 1
+  },
+  {
+    "id": 7573242285295693,
+    "name": "Nelson",
+    "gender": "male",
+    "age": 1,
+    "isNeutered": false,
+    "pictures": [
+      "",
+      ""
+    ],
+    "description": "Pecus deleo aetas corrumpo voluptates voluptas caput contego admitto arca arca cruciamentum bellicus assumenda certe censura officiis magni damnatio centum.",
+    "petData": 1
+  },
+  {
+    "id": 4633830215693110,
+    "name": "Valentine",
+    "gender": "female",
+    "age": 10,
+    "isNeutered": false,
+    "pictures": [
+      "",
+      "",
+      ""
+    ],
+    "description": "Eaque arguo quae debilito pecco umerus cibus titulus capillus nobis caute abeo tam ambitus vitium aegre tremo nesciunt solio textilis.",
+    "petData": 1
+  }
+]
+
+export const mockPetData = [
+  {
+    "id": 1,
+    "breed": "Greek Shepherd",
+    "animalGroup": "dog",
+    "pet": [
+      3873870507518866,
+      3101143660657795,
+      6163823489655316,
+      5925596435163416
+    ]
+  },
+  {
+    "id": 2,
+    "breed": "Japanese Bobtail",
+    "animalGroup": "cat",
+    "pet": []
+  },
+  {
+    "id": 3,
+    "breed": "Aztec Thrush",
+    "animalGroup": "bird",
+    "pet": []
+  },
+  {
+    "id": 4,
+    "breed": "American Fuzzy Lop",
+    "animalGroup": "rabbit",
+    "pet": []
+  },
+  {
+    "id": 5,
+    "breed": "Jumbo flying squid",
+    "animalGroup": "fish",
+    "pet": []
+  }
+]
+
+export const mockDogUrl = "https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190801_112134.jpg"
