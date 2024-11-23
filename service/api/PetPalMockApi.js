250ffb74b0fb9829de428b88f0fa5eb537986d64
dextersjw <dextersjw@gmail.com>
2024-11-23 08:12:54 +0800

    changed to using ip instead of localhost

--- service/api/PetPalMockApi.js
+++ service/api/PetPalMockApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost";
+const BASE_URL = "http://10.0.2.2";
 const PORT = ":3001";
 
 const petPalMockApi = axios.create({ baseURL: BASE_URL + PORT })
